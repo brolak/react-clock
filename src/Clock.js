493fe8b80e7059cee54b0c3a56259ac8e44c2913
Mati <matiborlak@gmail.com>
2017-04-06 15:03:18 +0300

    working pomodoro, youtube, finish sound

--- src/Clock.js
+++ src/Clock.js
@@ -1,44 +1,111 @@
 import React from 'react';
-import ElevationBorder from './ElevationBorder';
+import ReactAudioPlayer from 'react-audio-player';
+import YouTube from './YouTube';
 
 class Clock extends React.Component {
   constructor() {
     super();
     this.state = {
-    	date: new Date()
-    };
+    	min: 0,
+    	sec: 2,
+    	finished: false
+    }
+    this.startTime = this.startTime.bind(this);
+    this.stopTime = this.stopTime.bind(this);
+    this.restartTimeShort = this.restartTimeShort.bind(this);
+    this.restartTimeLong = this.restartTimeLong.bind(this);
   }
 
   tick() {
-    this.setState({
-      date: new Date()
-    })
+  	if(this.state.sec == 1 && this.state.min == 0){
+  		this.setState({
+      		min: 0,
+      		sec: 0,
+      		finished: true
+    	})
+    	const alarm = document.getElementById('alarm');
+    	alarm.play(); 
+    	this.stopTime();
+  	} else if(this.state.sec == 0){
+  		this.setState({
+      		min: this.state.min -=1,
+      		sec: 59
+    	})
+  	} else {
+	    this.setState({
+	      sec: this.state.sec -=1
+	    })
+	}
   }
 
-  componentDidMount() {
+  startTime() {
   	this.timerId = setInterval(
       () => this.tick(),
       1000
     );
   }
 
-  componentWillUnmount() {
+  stopTime() {
   	clearInterval(this.timerId);
   }
 
+  restartTimeShort() {
+  	this.setState({
+  		min: 5,
+  		sec: 0,
+  		finished: false
+  	})
+  	this.startTime();
+  }
+
+  restartTimeLong() {
+  	clearInterval(this.timerId);
+  	this.setState({
+  		min: 25,
+  		sec: 0,
+  		finished: false
+  	})
+  	this.startTime();
+  }
+  
+
   render() {
-  	var date = this.state.date;
-  	if (date.getMonth() > 5 && date.getFullYear() >= 2017){
-    return null;
+  	var sec = this.state.sec;
+  	var min = this.state.min;
+  	var displaySeconds = function(seconds) {
+	  	if(seconds < 10){
+	  		return "0"+seconds
+	  	} else {
+	  		return seconds;
+	  	}
   	}
-    return (
-      <div>
-        <h1>Hello, Cohort 4!</h1>
-        <ElevationBorder>
-        	<h2>It is {date.toLocaleTimeString()}.</h2>
-        </ElevationBorder>
-      </div>
-    );
+  	if(this.state.finished){
+  		return (
+  		<div>
+  			<h1>Pomodoro Clock</h1>
+  			<iframe width="560" height="315" src="https://www.youtube.com/embed/OpIQNxiKJoE?controls=0" frameborder="0"></iframe>
+  			<h3>Restart the timer:</h3>
+			<audio id="alarm" hidden>
+ 				<source src="pager.mp3" type="audio/mpeg" />
+ 			</audio>
+ 			
+        	<button onClick={this.restartTimeShort}>Restart 5</button>
+        	<button onClick={this.restartTimeLong}>Restart 25</button>
+        </div>
+  		)
+  	} else {
+	    return (
+	      <div>
+	      
+	        <h1>Pomodoro Clock</h1>
+	        <iframe width="560" height="315" src="https://www.youtube.com/embed/OpIQNxiKJoE?controls=0" frameborder="0"></iframe>
+	        <h2>{min}:{displaySeconds(sec)}</h2>
+	        <button onClick={this.startTime}>Start</button>
+	        <button onClick={this.stopTime}>Pause</button>
+
+	      </div>
+	    );
+    }
   }
 }
 
---
+++ src/YouTube.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const YouTube = null;
+// function YouTube() {
+// 	render() {
+// 		return (
+// 			<iframe width="560" height="315" src="https://www.youtube.com/embed/OpIQNxiKJoE" frameborder="0"></iframe>
+// 		)
+// 	}
+// }
+
+export default YouTube;
